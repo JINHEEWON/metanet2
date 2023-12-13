a4acdc554883ae72169c8fdf9f05194fe4f367fb
엉히킴 <cin05189@naver.com>
2023-12-13 14:33:17 +0900

    initial commit

---
+++ team2/src/main/java/com/team2/board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.team2.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+
+   @GetMapping("/")
+   public @ResponseBody String index() {
+      return "하이엉히";
+   }
+}
