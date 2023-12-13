e4fbd71e4770a259c81cc56380989fddc503339c
taehyung0519 <taehyung0519@naver.com>
2023-12-13 16:00:47 +0900

    initial setting

---
+++ team2/src/main/java/com/team2/member/model/MemberUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.team2.member.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.team2.member.service.IMemberService;
+
+
+@Component
+public class MemberUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private IMemberService memberService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member memberinfo = memberService.selectMember(username);
+		if(memberinfo==null) {
+			throw new UsernameNotFoundException("["+username+"]사용자가 없어요");
+		}
+		String[] roles = {"ROLE-USER","ROLE_ADMIN"}; //데이터베이스에서 조회했다고 가정.
+		List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList(roles);
+		
+		//return new User(memberinfo.getUserid(),"{noop}"+memberinfo.getPassword(),authorities);
+		return new MemberUserDetails(memberinfo.getUserid(), memberinfo.getPassword(), authorities, memberinfo.getEmail());
+	}
+
+}
