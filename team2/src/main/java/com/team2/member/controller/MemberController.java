b2204bb7be3a1e8761909f63f9e3a256a7c1c4ea
엉히킴 <cin05189@naver.com>
2023-12-13 14:56:14 +0900

    commit 1

--- team2/src/main/java/com/team2/member/controller/MemberController.java
+++ team2/src/main/java/com/team2/member/controller/MemberController.java
@@ -1,14 +1,14 @@
-package com.team2.board.controller;
+package com.team2.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class BoardController {
+public class MemberController {
 
-   @GetMapping("/")
+   @GetMapping("/member")
    public @ResponseBody String index() {
-      return "하이그대들"; 
+      return "하이그대들fffff"; 
    }
 }
