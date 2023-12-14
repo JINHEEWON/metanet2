c223315c0ba2deec05b24a134e7e33e6ba5fae10
godo129 <godo129@naver.com>
2023-12-14 13:48:33 +0900

    feat: 게시글, 댓글 삭제 추가

--- team2/src/main/java/com/team2/board/controller/BoardController.java
+++ team2/src/main/java/com/team2/board/controller/BoardController.java
@@ -1,12 +1,18 @@
 package com.team2.board.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.team2.board.model.BoardVO;
 import com.team2.board.model.ReplyVO;
 import com.team2.board.service.IBoardService;
+
+import jakarta.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +39,29 @@ public class BoardController {
 	public ReplyVO test2(@RequestBody ReplyVO reply) {
 		boardService.createReply(reply);
 		return reply;
+	}
+	
+	@DeleteMapping("/delete/{boardId}")
+	public String deleteBoard(@PathVariable int boardId) {
+		try {
+			boardService.deleteBoard(boardId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+		return "success";
+	}
+	
+	@DeleteMapping("/deleteReply/{replyId}")
+	public String deleteReply(@PathVariable int replyId) {
+		System.out.println(replyId);
+		try {
+			boardService.deleteReply(replyId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "fail";
+		}
+		return "success";
 	}
 
 }
