e8bf003124290176ec04521ea8be23fb967fb31a
godo129 <godo129@naver.com>
2023-12-14 17:53:29 +0900

    Feat: 수정시간 설정 추가

--- team2/src/main/java/com/team2/board/controller/BoardController.java
+++ team2/src/main/java/com/team2/board/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.team2.board.controller;
 
+import java.sql.Date;
+
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -32,13 +34,15 @@ public class BoardController {
 	
 	@PostMapping(value="/create") 
 	public BoardVO testing(@RequestBody BoardVO board) {
-		System.out.println(board);
+		board.setUpdateDate(currentTime());
+		board.setWriteDate(currentTime());
 		boardService.createBoard(board);
 		return board;
 	}
 
 	@PostMapping(value="/createReply") 
 	public ReplyVO test2(@RequestBody ReplyVO reply) {
+		reply.setWriteDate(currentTime());
 		boardService.createReply(reply);
 		return reply;
 	}
@@ -67,6 +71,7 @@ public class BoardController {
 
 	@PutMapping("/update/{boardId}")
 	public BoardVO putMethodName(@PathVariable int boardId, @RequestBody BoardVO board) {
+		board.setUpdateDate(currentTime());
 		boardService.updateBoard(board);
 		return board;
 	}
@@ -75,5 +80,13 @@ public class BoardController {
 	public ReplyVO updateReply(@PathVariable int replyId, @RequestBody ReplyVO reply) {
 		boardService.updateReply(reply);
 		return reply;
+	}
+	
+	private Date currentTime() {
+        // 현재 날짜와 시간 얻기
+        java.util.Date currentDate = new java.util.Date();
+        // java.util.Date를 java.sql.Date로 변환
+        Date sqlDate = new Date(currentDate.getTime());
+        return sqlDate;
 	}
 }
